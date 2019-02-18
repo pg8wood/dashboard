d558bafea7950c69f16216f3fee715880a939b54
Patrick Gatewood <pg8wood@vt.edu>
2019-02-18 12:26:02 -0500

    Create ServiceTableViewCell

--- Dashboard/Dashboard/ViewController.swift
+++ Dashboard/Dashboard/ViewController.swift
@@ -12,6 +12,23 @@ class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.register(UINib(nibName: "ServiceTableViewCell", bundle: nil), forCellReuseIdentifier: "ServiceTableViewCell")
+    }
+
+    // MARK: - TableView data source
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ServiceTableViewCell", for: indexPath) as! ServiceTableViewCell
+        cell.serviceImageView.image = UIImage(named: "ghost")
+        cell.serviceNameLabel.text = "Test service"
+        cell.statusImageView.image = UIImage(named: "server-error")
+        return cell
     }
 }
