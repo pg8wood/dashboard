ab5e6426b66100fa646ed1624c26ff4171bb186e
Patrick Gatewood <pg8wood@vt.edu>
2019-02-18 12:04:31 -0500

    Initial commit

---
+++ Dashboard/Dashboard/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Dashboard
+//
+//  Created by Patrick Gatewood on 2/18/19.
+//  Copyright © 2019 Patrick Gatewood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
