c1b5f0d9f327e34f7be69823c046a645f92da84d
Patrick Gatewood <pg8wood@vt.edu>
2019-02-20 01:00:06 -0500

    Add cancel button

--- Dashboard/Dashboard/New Group/AddServiceViewController.swift
+++ Dashboard/Dashboard/New Group/AddServiceViewController.swift
@@ -9,20 +9,12 @@
 import UIKit
 
 class AddServiceViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func cancelButtonTapped(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
     }
-    */
-
 }
