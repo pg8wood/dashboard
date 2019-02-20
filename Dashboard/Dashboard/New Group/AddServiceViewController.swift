87336b2b4a94fe121f96e2c95f6973365933c3ac
Patrick Gatewood <pg8wood@vt.edu>
2019-02-20 00:49:34 -0500

    Create add service skeleton

---
+++ Dashboard/Dashboard/New Group/AddServiceViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AddServiceViewController.swift
+//  Dashboard
+//
+//  Created by Patrick Gatewood on 2/19/19.
+//  Copyright © 2019 Patrick Gatewood. All rights reserved.
+//
+
+import UIKit
+
+class AddServiceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
