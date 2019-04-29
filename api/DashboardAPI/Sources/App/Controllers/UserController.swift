c1b721b32f1100fbc7244a9ce312f426ffcd1e7b
Patrick Gatewood <pg8wood@vt.edu>
2019-04-29 19:26:49 -0400

    Add User creation logic

---
+++ api/DashboardAPI/Sources/App/Controllers/UserController.swift
@@ -0,0 +1,17 @@
+import Vapor
+
+final class UserController {
+    
+    func createHandler(_ req: Request) throws -> Future<User> {
+        return try req.content.decode(User.self).flatMap { (user) in
+            return user.save(on: req)
+        }
+    }
+}
+
+extension UserController: RouteCollection {
+    func boot(router: Router) throws {
+        let pushRoute = router.grouped("user", "push-token")
+        pushRoute.post(User.parameter, use: createHandler)
+    }
+}
