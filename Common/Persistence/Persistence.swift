25f29da88b03f54f64b9099a710bc565501d719a
Patrick Gatewood <pg8wood@vt.edu>
2019-07-24 13:45:06 -0400

    Create NSFetchedResultsController

--- Common/Persistence/Persistence.swift
+++ Common/Persistence/Persistence.swift
@@ -10,7 +10,8 @@ import CoreData
 import UIKit
 
 protocol Database {
-    func getStoredServices(_ completion: @escaping (_ result: Result<[ServiceModel], Error>) -> Void)
+    var fetchedResultsController: NSFetchedResultsController<NSFetchRequestResult> { get }
+    
     func save(image: UIImage, named fileName: String)
     func renameFile(from oldFileName: String, to newFileName: String)
     func updateLastOnlineDate(for service: ServiceModel, lastOnline: Date)
@@ -20,14 +21,6 @@ class PersistenceClient {
     
     static let documentsUrl = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.com.willowtreeapps.patrick.gatewood.dashboard")!
     
-    let fileManager = FileManager.default
-    
-    static func fetchImage(named: String) -> UIImage? {
-        let imageFilePath = documentsUrl.appendingPathComponent("\(named)")
-        return UIImage(contentsOfFile: imageFilePath.path)
-    }
-    
-    // MARK: - Core Data stack
     static var persistentContainer: NSPersistentContainer = {
         let container = NSPersistentContainer(name: "Services")
         let storeUrl = documentsUrl.appendingPathComponent("services.sqlite")
@@ -40,23 +33,36 @@ class PersistenceClient {
         })
         return container
     }()
-}
-
-extension PersistenceClient: Database {
-    func getStoredServices(_ completion: @escaping (_ result: Result<[ServiceModel], Error>) -> Void) {
-        let managedContext = PersistenceClient.persistentContainer.viewContext
+    
+    private var fetchController: NSFetchedResultsController<NSFetchRequestResult>!
+    
+    init(delegate: NSFetchedResultsControllerDelegate) {
         let serviceFetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: ServiceModel.entityName)
+        let managedContext = PersistenceClient.persistentContainer.viewContext
         let sortDescriptor = NSSortDescriptor(keyPath: \ServiceModel.index, ascending: true)
         
         serviceFetchRequest.sortDescriptors = [sortDescriptor]
         
+        fetchController = NSFetchedResultsController(fetchRequest: serviceFetchRequest, managedObjectContext: managedContext, sectionNameKeyPath: nil, cacheName: nil)
+        fetchedResultsController.delegate = delegate
+        
         do {
-            // TODO create database error and use here instead of unsafe uwrap
-            let fetchedServices = try managedContext.fetch(serviceFetchRequest) as! [ServiceModel]
-            completion(.success(fetchedServices))
+            try fetchController.performFetch()
         } catch {
+            // TODO have delegate show error
             fatalError("Failed to fetch service models: \(error)")
         }
+    }
+    
+    static func fetchImage(named: String) -> UIImage? {
+        let imageFilePath = documentsUrl.appendingPathComponent("\(named)")
+        return UIImage(contentsOfFile: imageFilePath.path)
+    }
+}
+
+extension PersistenceClient: Database {
+    var fetchedResultsController: NSFetchedResultsController<NSFetchRequestResult> {
+        return fetchController
     }
     
     /**
@@ -69,10 +75,10 @@ extension PersistenceClient: Database {
         let imageFilePath = PersistenceClient.documentsUrl.appendingPathComponent("\(fileName)")
         
         do {
-            let files = try fileManager.contentsOfDirectory(atPath: PersistenceClient.documentsUrl.path)
+            let files = try FileManager.default.contentsOfDirectory(atPath: PersistenceClient.documentsUrl.path)
             
             if files.contains(imageFilePath.path) {
-                try fileManager.removeItem(atPath: imageFilePath.path)
+                try FileManager.default.removeItem(atPath: imageFilePath.path)
             }
             
             if let imagePngData = image.pngData() {
@@ -88,7 +94,7 @@ extension PersistenceClient: Database {
         let newImageFilePath = PersistenceClient.documentsUrl.appendingPathComponent("\(newFileName)")
         
         do {
-            try fileManager.moveItem(at: oldImageFilePath, to: newImageFilePath)
+            try FileManager.default.moveItem(at: oldImageFilePath, to: newImageFilePath)
         } catch {
             print("Failed to rename image. Saving new instead")
         }
